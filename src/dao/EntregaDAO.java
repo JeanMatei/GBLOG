8e1a7d30efb3f29da395556dc1052141db1cebde
JeanMatei <luxhemallow@gmail.com>
2025-02-18 21:24:46 -0300

    .

--- src/dao/EntregaDAO.java
+++ src/dao/EntregaDAO.java
@@ -2,9 +2,12 @@ package dao;
 
 import connection.ConexaoMySQL;
 import model.Entrega;
+import model.Filial;
+import model.Veiculo;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class EntregaDAO implements DAO<Entrega> {
@@ -12,11 +15,11 @@ public class EntregaDAO implements DAO<Entrega> {
     public ArrayList<Entrega> selecionar() throws Exception {
         try {
             String sql = "SELECT" +
-                    "id, " +
-                    "nome_completo, " +
-                    "data_nascimento," +
-                    "documento, " +
-                    "pais, " +
+                    "cdEntrega, " +
+                    "origem, " +
+                    "destino," +
+                    "nmCliente, " +
+                    "nmDestinatario     , " +
                     "estado, " +
                     "cidade " +
                     "FROM pessoa";
@@ -26,3 +29,14 @@ public class EntregaDAO implements DAO<Entrega> {
         }
     }
 }
+private String cdEntrega;
+private Filial origem;
+private Filial destino;
+private String nmCliente;
+private String nmDestinatario;
+private String descricao;
+private Double ptcarga;
+private Veiculo veiculo; // Veículo designado
+private String status;
+private LocalDateTime saida;
+private LocalDateTime chegada;
