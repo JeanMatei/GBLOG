1baa403586e9b75301f2881ae3788afabdab7dbf
JeanMatei <luxhemallow@gmail.com>
2025-02-18 21:14:07 -0300

    .

--- src/service/EntregaService.java
+++ src/service/EntregaService.java
@@ -1,5 +1,14 @@
 package service;
 
+import dao.EntregaDAO;
+
 public class EntregaService {
+
+private EntregaDAO(EntregaDAO entregaDAO){ this.entregaDAO = entregaDAO;}
+
+    public String listar()throws Exception{}
+
+
+
 
 }
