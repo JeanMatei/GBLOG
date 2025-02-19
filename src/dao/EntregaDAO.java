b6fe7560d2b593244e08c48c15fbc7e60c26978e
JeanMatei <luxhemallow@gmail.com>
2025-02-18 21:21:53 -0300

    .

--- src/dao/EntregaDAO.java
+++ src/dao/EntregaDAO.java
@@ -1,4 +1,28 @@
 package dao;
 
-public class EntregaDAO {
+import connection.ConexaoMySQL;
+import model.Entrega;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class EntregaDAO implements DAO<Entrega> {
+
+    public ArrayList<Entrega> selecionar() throws Exception {
+        try {
+            String sql = "SELECT" +
+                    "id, " +
+                    "nome_completo, " +
+                    "data_nascimento," +
+                    "documento, " +
+                    "pais, " +
+                    "estado, " +
+                    "cidade " +
+                    "FROM pessoa";
+            Statement declaracao = ConexaoMySQL.get().createStatement();
+            ResultSet resultado = declaracao.executeQuery(sql);
+
+        }
+    }
 }
--- src/model/Identificavel.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface Identificavel {
-    String getCodigoUnico();
-}
