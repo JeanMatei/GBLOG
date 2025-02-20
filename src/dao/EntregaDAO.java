182debf5561363b36b08cdb2be850198df9e4638
JeanMatei <luxhemallow@gmail.com>
2025-02-19 22:24:58 -0300

    .

--- src/dao/EntregaDAO.java
+++ src/dao/EntregaDAO.java
@@ -2,22 +2,25 @@ package dao;
 
 import connection.ConexaoMySQL;
 import model.Entrega;
+import model.Filial;
+import model.Veiculo;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class EntregaDAO implements DAO<Entrega> {
+public class EntregaDAO  {
 
     public ArrayList<Entrega> selecionar() throws Exception {
         try {
-            String sql = "SELECT" +
+            String sql = "SELECT " +
                     "cdEntrega, " +
                     "origem, " +
                     "destino," +
                     "nmCliente, " +
-                    "nmDestinatario     , " +
+                    "nmDestinatario, " +
                     "descricao, " +
                     "ptcarga " +
                     "veiculo" +
@@ -31,10 +34,33 @@ public class EntregaDAO implements DAO<Entrega> {
             ArrayList<Entrega> entregas = new ArrayList<>();
             while (resultado.next()) {
                 Entrega entrega = new Entrega(
-                        resultado.getString("id"),
-                        resultado.getString()
+                        resultado.getString("cdentrega"),
+                        resultado.getString("origem"),
+                        resultado.getString("destino"),
+                        resultado.getString("nmCliente"),
+                        resultado.getString("nmDestinatario"),
+                        resultado.getString("descricao"),
+                        resultado.getDouble("ptcarga"),
+                        resultado.getString("veiculo"),
+                        resultado.getString("status"),
+                        resultado.getDate("saida").toLocalDate(),
+                        resultado.getDate("chegada").toLocalDate()
                         );
                 entregas.add(entrega);
+                private String cdEntrega;
+                private Filial origem;
+                private Filial destino;
+                private String nmCliente;
+                private String nmDestinatario;
+                private String descricao;
+                private Double ptcarga;
+                private Veiculo veiculo; // Veículo designado
+                private String status;
+                private LocalDateTime saida;
+                private LocalDateTime chegada;
+
+
+
             }
             return entregas;
 
